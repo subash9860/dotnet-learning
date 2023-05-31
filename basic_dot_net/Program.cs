84939160be5068e109f705ed7fe2765a20c43aa5
subash kc <subaazkc@gmail.com>
2023-05-31 16:27:03 +0545

    first commit

---
+++ basic_dot_net/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // printing a line
+            Console.WriteLine("What is your name?");
+            // reading a line form the user
+            var name = Console.ReadLine();
+
+            var currentDate = DateTime.Now;
+
+            // string interpolation
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+
+            // holding a key for termination
+            Console.ReadKey(true);
+
+        }
+    }
+}
